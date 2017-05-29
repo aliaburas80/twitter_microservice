67d57e00811cdb3e2e3f33444771c4839e440b9b
Ali Abu Ras <aliaburas80@gmail.com>
2017-05-29 18:56:39 +0300

    add wait.js and new implementation

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-let tinyService = require('./src/service/tinyLink');
-let printWait = require('./src/service/wait');
-
+let tinyService    = require('./src/service/tinyLink');
+let printWait      = require('./src/service/wait');
+let config         = require('./src/service/nconf')
 let postTweetChain = require('post-tweet-chain');
 let Twit = require('twit');
 let tinyURLS= [];
@@ -16,6 +16,11 @@ let urls = [
     ];
 
 let isDone = false;
+let nconf = require('nconf');
+
+nconf.argv().env();
+nconf.file({ file: './config/config.json' });
+
 
 let doPrint = ()=>{
   if(!isDone){
@@ -29,6 +34,9 @@ let doPrint = ()=>{
 let doFinish = ()=>{
   printWait.doFinish();
 }
+
+
+
 
 printWait.virtualConsole;
 doPrint();
@@ -45,16 +53,16 @@ tinyService(urls).then(
 
 
 
-
 let twit = new Twit({
-  consumer_key:         '9cQWIozM5Kg9T9hqmq4fs2VeZ',
-  consumer_secret:      '5mCW4tcSX8dAE2R9dCUOCNac4Bcg1jivJju9STvJAqMnngy11z',
-  access_token:         '234758171-OKUq3q22KtVUl48oqELbBAPchyhGeANcbSFyqTtI',
-  access_token_secret:  'Yo1EKGmCenLUG7medCETtadiEvoQceQltfIyOTFg3wp4F'
+  consumer_key:         nconf.get('consumer_key'),
+  consumer_secret:      nconf.get('consumer_secret'),
+  access_token:         nconf.get('access_token'),
+  access_token_secret:  nconf.get('access_token_secret')
 });
 
 let sendTweet = ()=>{
   for(let link of tinyURLS){
+
     let opts1 = {
       twit: twit,
       parts: [
@@ -71,6 +79,7 @@ let sendTweet = ()=>{
         '#أردني_يازين_الهوية #الأردن #القوات_المسلحة_الأردنية #الجيش_العربي #امانة_عمان @RadioHalaJO'
       ]
     };
+
       postTweetChain(opts1, logDone);
       postTweetChain(opts2, logDone);
 
---
+++ src/service/wait.js
@@ -0,0 +1,47 @@
+let virtualConsole = {
+    lines: [],
+    currentLine: 0,
+    log:  (msg, appendToCurrentLine)=> {
+        if (!appendToCurrentLine) virtualConsole.currentLine++;
+        if (appendToCurrentLine && virtualConsole.lines[virtualConsole.currentLine]) {
+            virtualConsole.lines[virtualConsole.currentLine] += msg;
+        } else {
+            virtualConsole.lines[virtualConsole.currentLine] = msg;
+        }
+
+          process.stdout.clearLine();  // clear current text
+          process.stdout.cursorTo(0);  // move cursor to beginning of line
+
+        virtualConsole.lines.forEach(function (line) {
+            process.stdout.write(line);  // write text
+        });
+    },
+    clear:  ()=> {
+      process.stdout.clearLine();  // clear current text
+      process.stdout.cursorTo(0);  // move cursor to beginning of line
+      virtualConsole.currentLine = 0;
+    }
+}
+
+// =================================================
+// Little demo to demonstrate how it looks.
+// =================================================
+
+// Write an initial console entry.
+virtualConsole.log("Wait");
+
+// Append to last line a few times.
+
+let doWait = ()=>{
+  virtualConsole.log(".", true); // <- Append.
+}
+
+// Write a new line.
+let doFinish= ()=>{
+  virtualConsole.clear();
+  virtualConsole.log("\nFinished.\n"); // <- New line.
+}
+
+module.exports = {
+doWait,doFinish
+};
