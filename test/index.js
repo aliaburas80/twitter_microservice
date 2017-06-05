a03899ef854a94d998b55b5fac43a2b122c0afd4
AliAbuRas <alaburas@mbc.net>
2017-06-05 10:55:11 +0300

    add test file

---
+++ test/index.js
@@ -0,0 +1,13 @@
+let tw = require('twitter-service');
+
+tw(twitter:{
+    "consumer_key"       :"9cQWIozM5Kg9T9hqmq4fs2VeZ",
+    "consumer_secret"    :"5mCW4tcSX8dAE2R9dCUOCNac4Bcg1jivJju9STvJAqMnngy11z",
+    "access_token"       :"234758171-OKUq3q22KtVUl48oqELbBAPchyhGeANcbSFyqTtI",
+    "access_token_secret":"Yo1EKGmCenLUG7medCETtadiEvoQceQltfIyOTFg3wp4F"
+  },
+  links :{[
+      'https://github.com/aliaburas80/twitter_microservice'
+    ]},
+  hashtags:"#github#photooftheday#photooftheday#beautiful#fashion#instagood#love#loveit#productivity#traveltips#travelfriendly#wheretonext#weddinginvitations#weddingregistry#dreamwedding"
+);
