d3a39e4ba9147d753de6529cde6fdaf7e845ea2e
Ali Abu Ras <aliaburas80@gmail.com>
2017-08-05 01:06:26 +0300

    Add end post event

--- src/events/Event.js
+++ src/events/Event.js
@@ -2,15 +2,19 @@ const EventEmitter = require('events');
 
 class MyEmitter extends EventEmitter {}
 const serviceEvents = new MyEmitter();
-
+let allowPost = true;
 
 process.on('errors', (message) => {
     console.log(`an error occurred!  ${message}`);
 });
 
 process.on('post', (message) => {
-
     console.log(`an post occurred!  ${message}`);
 });
 
-module.exports = serviceEvents
+process.on('stopPost', (message) => {
+    allowPost = false;
+    console.log('stop post');
+});
+
+module.exports = allowPost;
