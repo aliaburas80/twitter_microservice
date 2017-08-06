074bf4025c03bace91f7df54cdd3be7af5067e00
AliAbuRas <alaburas@mbc.net>
2017-08-06 10:39:19 +0300

    -fix error

--- src/service/twitter/post-tweets.js
+++ src/service/twitter/post-tweets.js
@@ -1,7 +1,6 @@
 /*
 Ali Abu Ras aliaburas80@gmail.com
 */
-
 /*just post tweets*/
 let events = require('../../events/Event');
 let postTweetChain = require('post-tweet-chain');
@@ -10,9 +9,7 @@ let messages=[];
 let totalDoneMessages = 0;
 let totalDuplicateMessages = 0;
 let twit;
-
 //
-
 module.exports  = (t,msg)=>{
     twit = t;
     messages = msg;
@@ -20,28 +17,28 @@ module.exports  = (t,msg)=>{
 }
 
 let logDone = ()=> {
-  index++;
-  if(index>=messages.length){
-    console.log('Totale done posts          '+( totalDoneMessages - totalDuplicateMessages ));
-    console.log('Totale duplicate messages  '+ totalDuplicateMessages);
-    return;
-  }else if(events.allowPost){
-    postTweet();
-  }
+    index++;
+    if(index>=messages.length){
+        console.log('Totale done posts          '+( totalDoneMessages - totalDuplicateMessages ));
+        console.log('Totale duplicate messages  '+ totalDuplicateMessages);
+        return;
+    }else if(events){
+        postTweet();
+    }
 }
 
 let postTweet = ()=>{
     twit.post('statuses/update', { status: messages[index] }, function(err, data, response) {
-      if(err){
-        let errorMessage = `Error number = ${err.allErrors[0].code}, ${err.allErrors[0].message}`;
-        process.emit('errors',errorMessage);
-        totalDuplicateMessages++;
-      }else{
-          process.emit('post',`${ messages[index]}`);
-      }
-      if(data){
-        totalDoneMessages++;
-      }
-    logDone();
-  });
+        if(err){
+            let errorMessage = `Error number = ${err.allErrors[0].code}, ${err.allErrors[0].message}`;
+            process.emit('errors',errorMessage);
+            totalDuplicateMessages++;
+        }else{
+            process.emit('post',`${ messages[index]}`);
+        }
+        if(data){
+            totalDoneMessages++;
+        }
+        logDone();
+    });
 }
